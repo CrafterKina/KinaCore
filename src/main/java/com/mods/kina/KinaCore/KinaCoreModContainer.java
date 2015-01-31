bace5ab84848c61a0460b5dc2bcbf886bef0c577
CrafterKina <kina_ugo@yahoo.co.jp>
2015-01-31 22:01:04 +0900

    1.8com

---
+++ src/main/java/com/mods/kina/KinaCore/KinaCoreModContainer.java
@@ -0,0 +1,22 @@
+/*package com.mods.kina.KinaCore;
+
+import com.google.common.eventbus.EventBus;
+import cpw.mods.fml.common.DummyModContainer;
+import cpw.mods.fml.common.LoadController;
+import cpw.mods.fml.common.ModMetadata;
+
+public class KinaCoreModContainer extends DummyModContainer{
+    public KinaCoreModContainer()
+    {
+        super(new ModMetadata());
+        ModMetadata meta = getMetadata();
+        meta.modId       = "kina_core";
+        this.setEnabledState(true);
+    }
+    @Override
+    public boolean registerBus(EventBus bus, LoadController controller)
+    {
+        bus.register(this);
+        return true;
+    }
+}*/
