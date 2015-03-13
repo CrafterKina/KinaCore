820482c183c0a0e0828a9494fdd8acfdd16bd041
CrafterKina <kina_ugo@yahoo.co.jp>
2015-03-13 09:44:50 +0900

    added post event

--- src/main/java/com/mods/kina/KinaCore/asm/transformer/FurnaceEventTransformer.java
+++ src/main/java/com/mods/kina/KinaCore/asm/transformer/FurnaceEventTransformer.java
@@ -22,9 +22,34 @@ public class FurnaceEventTransformer implements IClassTransformer, Opcodes{
     private ClassVisitor furnaceClassVisitor(ClassWriter writer){
         return new ClassVisitor(ASM4, writer){
             public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions){
-                if(!"update".equals(name) || !"()V".equals(desc))
-                    return super.visitMethod(access, name, desc, signature, exceptions);
-                return onUpdateMethodVisitor(api, super.visitMethod(access, name, desc, signature, exceptions));
+                if("update".equals(name) && "()V".equals(desc))
+                    return onUpdateMethodVisitor(api, super.visitMethod(access, name, desc, signature, exceptions));
+                if("smeltItem".equals(name) && "()V".equals(desc))
+                    return smeltItemMethodVisitor(api, super.visitMethod(access, name, desc, signature, exceptions));
+                return super.visitMethod(access, name, desc, signature, exceptions);
+            }
+        };
+    }
+
+    private MethodVisitor smeltItemMethodVisitor(int api, MethodVisitor methodVisitor){
+        return new MethodVisitor(api, methodVisitor){
+            boolean isub = false;
+            boolean isFirst = true;
+
+            public void visitInsn(int opcode){
+                if(opcode == ISUB && isFirst) isub = true;
+                super.visitInsn(opcode);
+            }
+
+            public void visitFieldInsn(int opcode, String owner, String name, String desc){
+                if(isub && isFirst && "stackSize".equals(name) && opcode == PUTFIELD){
+                    isFirst = false;
+                    super.visitFieldInsn(PUTFIELD, "net/minecraft/item/ItemStack", "stackSize", "I");
+                    super.visitVarInsn(ALOAD, 0);
+                    super.visitMethodInsn(INVOKESTATIC, "com/mods/kina/KinaCore/event/KinaCoreEventFactory", "onItemSmelted", "(Lnet/minecraft/tileentity/TileEntityFurnace;)V", false);
+                }else{
+                    super.visitFieldInsn(opcode, owner, name, desc);
+                }
             }
         };
     }
--- src/main/java/com/mods/kina/KinaCore/event/KinaCoreEventFactory.java
+++ src/main/java/com/mods/kina/KinaCore/event/KinaCoreEventFactory.java
@@ -6,6 +6,8 @@ import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
+import java.util.Arrays;
+
 public class KinaCoreEventFactory{
     public static void onItemAttemptSmelt(TileEntityFurnace furnace){
         ItemStack[] stacks = ReflectionHelper.getPrivateValue(TileEntityFurnace.class, furnace, "furnaceItemStacks");
@@ -16,5 +18,13 @@ public class KinaCoreEventFactory{
         ReflectionHelper.setPrivateValue(TileEntityFurnace.class, furnace, event.getStacks(), "furnaceItemStacks");
         ReflectionHelper.setPrivateValue(TileEntityFurnace.class, furnace, event.getCookTime(), "cookTime");
         ReflectionHelper.setPrivateValue(TileEntityFurnace.class, furnace, event.getTotalCookTime(), "totalCookTime");
+    }
+
+    public static void onItemSmelted(TileEntityFurnace furnace){
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        ItemStack[] stacks = ReflectionHelper.getPrivateValue(TileEntityFurnace.class, furnace, "furnaceItemStacks");
+        ItemSmeltEvent.Post event = new ItemSmeltEvent.Post(furnace.getWorld(), furnace.getPos(), stacks);
+        MinecraftForge.EVENT_BUS.post(event);
+        ReflectionHelper.setPrivateValue(TileEntityFurnace.class, furnace, event.getStacks(), "furnaceItemStacks");
     }
 }
