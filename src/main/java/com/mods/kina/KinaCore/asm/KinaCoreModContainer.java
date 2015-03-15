c07825c887e939ed3028e03df7f7c9f9b61fc14a
CrafterKina <kina_ugo@yahoo.co.jp>
2015-03-15 10:23:08 +0900

    close #5 Modlist覗くとクラッシュする

--- src/main/java/com/mods/kina/KinaCore/asm/KinaCoreModContainer.java
+++ src/main/java/com/mods/kina/KinaCore/asm/KinaCoreModContainer.java
@@ -5,11 +5,19 @@ import net.minecraftforge.fml.common.DummyModContainer;
 import net.minecraftforge.fml.common.LoadController;
 import net.minecraftforge.fml.common.ModMetadata;
 
+import java.util.Arrays;
+
 public class KinaCoreModContainer extends DummyModContainer{
     public KinaCoreModContainer(){
         super(new ModMetadata());
         ModMetadata meta = getMetadata();
         meta.modId = "kina_core";
+        meta.name = "KinaCore";
+        meta.version = "@version@";
+        meta.authorList = Arrays.asList("CrafterKina");
+        meta.description = "";
+        meta.url = "";
+        meta.credits = "";
         setEnabledState(true);
     }
 
